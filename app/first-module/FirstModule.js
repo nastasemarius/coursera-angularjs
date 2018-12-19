d7cfc09b905f5e1a2962608cfabcbf28945c37bf
Nastase Marius <nastase.marius94@gmail.com>
2018-12-19 15:38:22 +0200

    First assignment completed

--- app/first-module/FirstModule.js
+++ app/first-module/FirstModule.js
@@ -4,13 +4,49 @@
     angular.module('app')
         .controller('FirstModule', FirstModule);
 
-    FirstModule.$inject = [];
+    FirstModule.$inject = ['$scope'];
 
-    function FirstModule() {
+    function FirstModule($scope) {
         var vm = this;
+        vm.checkFoodAmount = checkFoodAmount;
 
-        vm.someData = {
-            text: 'Some random text in here'
-        };
+        activate();
+
+        function activate() {
+            $scope.$watch('vm.foodList', watchFoodList);
+        }
+
+        function checkFoodAmount() {
+            var parsedFoodList = parseFoodList(vm.foodList);
+            vm.message = evaluateFoodAmount(parsedFoodList.length);
+        }
+
+        function parseFoodList(foodList) {
+            if (typeof foodList === 'string') {
+                return foodList
+                    .split(',')
+                    .filter(filterListItems);
+            } else return [];
+
+            function filterListItems(listItem) {
+                return !!listItem
+            }
+        }
+
+        function evaluateFoodAmount(amount) {
+            if (amount > 0 && amount < 3) {
+                return 'Not enough calories. Get some more food!'
+            } else if (amount >=3  && amount < 5) {
+                return 'You had enough too eat.'
+            } else if (amount >= 5) {
+                return 'You ate way too much. You are going to get fat!'
+            }
+        }
+
+        function watchFoodList(newValue) {
+            if (!newValue) {
+                vm.message = '';
+            }
+        }
     }
 }());
