63612dd531f89d583399e8544241acf266e7b480
Nastase Marius <nastase.marius94@gmail.com>
2018-12-19 15:42:27 +0200

    Folder structure changed

--- app/router.config.js
+++ app/router.config.js
@@ -9,14 +9,14 @@
             url: '/module-1',
             controller: 'FirstModule',
             controllerAs: 'vm',
-            templateUrl: 'first-module/FirstModule.html'
+            templateUrl: 'app/first-module/FirstModule.html'
         });
 
         $stateProvider.state('module-2',{
             url:'/module-2',
             controller: 'SecondModule',
             controllerAs: 'vm',
-            templateUrl: 'second-module/SecondModule.html'
+            templateUrl: 'app/second-module/SecondModule.html'
         })
 
         $urlRouterProvider.otherwise('/module-1');
